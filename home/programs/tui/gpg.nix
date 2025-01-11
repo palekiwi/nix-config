3253a754d38f9d11c11634cfef8b405109b2cc4b
Pawel Lisewski <dev@palekiwi.com>
2025-01-11 23:02:19 +0800

    Write gpg-agent.conf

--- home/programs/tui/gpg.nix
+++ home/programs/tui/gpg.nix
@@ -1,7 +1,11 @@
 { ... }:
 
 {
-  home.file.".gnupg/gpg-agent.conf" = {
-    source = ../../../config/gnupg/gpg-agent.conf;
-  };
+  home.file.".gnupg/gpg-agent.conf".text = ''
+    enable-ssh-support
+    ttyname $GPG_TTY
+    default-cache-ttl 60
+    max-cache-ttl 120
+    pinentry-program /usr/bin/pinentry-gnome3
+  '';
 }
