c672405a941a0552b649150a13ce8a3f76f5b473
Pawel Lisewski <dev@palekiwi.com>
2025-01-17 23:39:02 +0800

    Add alsa-utils to hosts/sayuri

--- hosts/sayuri/default.nix
+++ hosts/sayuri/default.nix
@@ -135,6 +135,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    alsa-utils
     awesome
     git
     gnupg
