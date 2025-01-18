d85b46a2a95a72582dddb2ce95c01d68b589a112
Pawel Lisewski <dev@palekiwi.com>
2025-01-18 18:15:13 +0800

    Update ssh hosts

--- home/programs/tui/ssh.nix
+++ home/programs/tui/ssh.nix
@@ -2,16 +2,23 @@
 let
   user = "pl";
   port = "438";
-  hosts = [ "haze" "pale" ];
+  hosts = [
+    "akemi"
+    "haze"
+    "pale"
+    "sayuri"
+  ];
 in
 {
   programs.ssh = {
     enable = true;
 
-    extraConfig = lib.concatMapStrings (host: ''
-      Host ${host}
-        user ${user}
-        port ${port}
-    '') hosts;
+    extraConfig = lib.concatMapStrings
+      (host: ''
+        Host ${host}
+          user ${user}
+          port ${port}
+      '')
+      hosts;
   };
 }
