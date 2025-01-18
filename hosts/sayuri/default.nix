ff87ce62d151eaa0bc0a2ad8a887a07acc2266d4
Pawel Lisewski <dev@palekiwi.com>
2025-01-18 14:24:01 +0800

    move more settings to system

--- hosts/sayuri/default.nix
+++ hosts/sayuri/default.nix
@@ -66,60 +66,6 @@
     layout = "us";
     variant = "";
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.pl = {
-    isNormalUser = true;
-    description = "pl";
-    extraGroups = [ "docker" "networkmanager" "wheel" ];
-    shell = pkgs.zsh;
-  };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-  programs.zsh.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    awesome
-    git
-    gnupg
-    neovim
-    picom
-    pinentry-gnome3
-    rsync
-    wget
-    xorg.xrandr
-    xscreensaver
-    zsh
-  ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -168,6 +114,12 @@
     pinentryPackage = pkgs.pinentry-gnome3;
   };
 
+  programs.firefox.enable = true;
+  programs.zsh.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
@@ -181,6 +133,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
-
-  virtualisation.docker.enable = true;
 }
