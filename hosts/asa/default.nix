96cd8a0dc91596a2d3638b7dd25dee96dfaf60aa
Pawel Lisewski <dev@palekiwi.com>
2025-01-30 22:28:01 +0800

    change autologin to bb

--- hosts/asa/default.nix
+++ hosts/asa/default.nix
@@ -83,7 +83,7 @@
 
   # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "pl";
+  services.xserver.displayManager.autoLogin.user = "bb";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
