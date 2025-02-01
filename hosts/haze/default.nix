ad7f540bd80fd6f7a087214245214bb9ce95de85
Pawel Lisewski <dev@palekiwi.com>
2025-02-01 22:42:47 +0800

    use server module

--- hosts/haze/default.nix
+++ hosts/haze/default.nix
@@ -1,64 +1,39 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   imports =
     [
       ./hardware-configuration.nix
+      ../../modules/docker.nix
+      ../../modules/server.nix
     ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  config = {
+    modules.docker.enable = false;
 
-  networking.hostName = "haze";
-  networking.networkmanager.enable = true;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  time.timeZone = "Asia/Taipei";
+    networking.hostName = "haze";
 
-  i18n.defaultLocale = "en_US.UTF-8";
+    networking.networkmanager.enable = true;
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+    services.xserver.xkb = {
+      layout = "us";
+      variant = "";
+    };
+
+    users.users.pl.extraGroups = [ "dialout" ];
+
+    services.udev.extraRules = ''
+      KERNEL=="ttyUSB0", OWNER="pl"
+    '';
+
+    networking.firewall.allowedTCPPorts = [ 8080 8123 5050 ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+
+    virtualisation.podman.enable = true;
+
+    system.stateVersion = "24.11";
   };
-
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  users.users.pl = {
-    isNormalUser = true;
-    description = "pl";
-    extraGroups = [ "networkmanager" "wheel" ];
-  };
-
-  environment.systemPackages = with pkgs; [
-    curl
-    git
-    gitui
-    neovim
-    rsync
-    wget
-  ];
-
-  services.openssh.enable = true;
-
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
 }
--- modules/server.nix
+++ modules/server.nix
@@ -64,6 +64,7 @@
   environment.systemPackages = with pkgs; [
     curl
     git
+    gitui
     gnupg
     lm_sensors
     neovim
