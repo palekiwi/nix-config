a6365aada6572f3f982187d1cc7f4f5f2aa9ee85
Pawel Lisewski <dev@palekiwi.com>
2025-02-09 23:37:36 +0800

    set home-manager to 24.11

--- home/flake.nix
+++ home/flake.nix
@@ -2,9 +2,9 @@
   description = "Home Manager Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
