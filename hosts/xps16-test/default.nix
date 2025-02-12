1fb9d9b2346c4f6e3349a6ade5c26acc171082fe
Pawel Lisewski <dev@palekiwi.com>
2025-02-12 18:24:08 +0800

    update to latest kernel

--- hosts/xps16-test/default.nix
+++ hosts/xps16-test/default.nix
@@ -13,6 +13,8 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
 
   networking.hostName = "xps16-test"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
