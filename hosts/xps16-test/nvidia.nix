5243e8274ee09ee3ae7c6d92abc6f6595af03d48
Pawel Lisewski <dev@palekiwi.com>
2025-02-15 11:26:03 +0800

    Add busIds

--- hosts/xps16-test/nvidia.nix
+++ hosts/xps16-test/nvidia.nix
@@ -39,4 +39,9 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+
+  hardware.nvidia.prime = {
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
 }
