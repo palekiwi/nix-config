705cccae5234ae7d071c22dd54ec7141b45a539a
Pawel Lisewski <dev@palekiwi.com>
2025-02-17 14:51:41 +0800

    dmenu_tmux add stable

--- home/scripts/dmenu/dmenu_tmux.sh
+++ home/scripts/dmenu/dmenu_tmux.sh
@@ -5,7 +5,7 @@ launcher="rofi -dmenu -i"
 options=$(sesh list --json \
     | jq -r '.[] | (.Score|tostring) + "," + .Name + "," + .Src + "," + .Path + "," + (.Attached | if . > 0 then "*" else " " end)' \
     | sort -k 2,2 -k 4,4 -t"," --stable --unique \
-    | sort -nk 1 -t"," \
+    | sort -nk 1 -t"," --stable \
     | cut -d',' -f2- \
     | column -s"," -t)
 
