025fb59684b39fd484a1b432f528c5912ca8c230
Pawel Lisewski <dev@palekiwi.com>
2025-05-06 11:11:55 +0800

    xorg: set Menu as Compose in xmodmap

--- home/programs/gui/xorg.nix
+++ home/programs/gui/xorg.nix
@@ -10,6 +10,10 @@
 
   home.file.".Xmodmap".text = ''
     pointer = 1 3 2 4 5 7 6 8 9 10 11 12
+
     keycode 192 = ISO_Level3_Shift
+
+    # Menu as compose key
+    keycode 138 = Multi_key
   '';
 }
