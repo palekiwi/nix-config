927054d347055fad044584789aa860f9c6c6b171
Pawel Lisewski <dev@palekiwi.com>
2025-05-10 15:49:07 +0800

    Add theme and macros

--- home/programs/tui/email.nix
+++ home/programs/tui/email.nix
@@ -25,6 +25,56 @@
       settings = {
         mail_check_stats = "yes";
       };
+
+      extraConfig = ''
+        macro index,pager gi "<change-folder>=Inbox<Enter>" "Go to inbox"
+
+        macro index,pager \cb "<pipe-message> ${pkgs.urlscan}/bin/urlscan<Enter>" "Extract URLs"
+
+        # Dracula Theme for NeoMutt
+
+        # General colors
+        color normal default default
+        color error brightred default
+        color tilde black default
+        color message cyan default
+        color markers brightred white
+        color attachment brightmagenta default
+        color search brightgreen default
+        color status brightwhite black
+        color indicator black cyan
+        color tree yellow default
+
+        # Index colors
+        color index cyan default ~N            # New messages
+        color index blue default ~O            # Old messages
+        color index brightred default ~D       # Deleted messages
+        color index brightgreen default ~F     # Flagged messages
+        color index brightyellow default ~T    # Tagged messages
+
+        # Message headers
+        color hdrdefault cyan default
+        color header brightgreen default "^From:"
+        color header brightblue default "^Subject:"
+        color header brightmagenta default "^Date:"
+
+        # Body colors
+        color quoted green default
+        color quoted1 blue default
+        color quoted2 magenta default
+        color quoted3 cyan default
+        color quoted4 yellow default
+
+        # URL colors
+        color body brightblue default "([a-z][a-z0-9+-]*://(((([a-z0-9_.!~*'();:&=+$,-]|%[0-9a-f][0-9a-f])*@)?((([a-z0-9]([a-z0-9-]*[a-z0-9])?)\\.)*([a-z]([a-z0-9-]*[a-z0-9])?)\\.?|[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+)(:[0-9]+)?)|([a-z0-9_.!~*'()$,;:@&=+-]|%[0-9a-f][0-9a-f])+)(/([a-z0-9_.!~*'():@&=+$,-]|%[0-9a-f][0-9a-f])*(;([a-z0-9_.!~*'():@&=+$,-]|%[0-9a-f][0-9a-f])*)*(/([a-z0-9_.!~*'():@&=+$,-]|%[0-9a-f][0-9a-f])*(;([a-z0-9_.!~*'():@&=+$,-]|%[0-9a-f][0-9a-f])*)*)*)?(\\?([a-z0-9_.!~*'();/?:@&=+$,-]|%[0-9a-f][0-9a-f])*)?(#([a-z0-9_.!~*'();/?:@&=+$,-]|%[0-9a-f][0-9a-f])*)?|(www|ftp)\\.(([a-z0-9]([a-z0-9-]*[a-z0-9])?)\\.)*([a-z]([a-z0-9-]*[a-z0-9])?)\\.?(:[0-9]+)?(/([-a-z0-9_.!~*'():@&=+$,]|%[0-9a-f][0-9a-f])*(;([-a-z0-9_.!~*'():@&=+$,]|%[0-9a-f][0-9a-f])*)*(/([-a-z0-9_.!~*'():@&=+$,]|%[0-9a-f][0-9a-f])*(;([-a-z0-9_.!~*'():@&=+$,]|%[0-9a-f][0-9a-f])*)*)*)?(\\?([-a-z0-9_.!~*'();/?:@&=+$,]|%[0-9a-f][0-9a-f])*)?(#([-a-z0-9_.!~*'();/?:@&=+$,]|%[0-9a-f][0-9a-f])*)?)[^].,:;!)? \t\r\n<>\"]"
+
+        # Sidebar colors
+        color sidebar_indicator black cyan
+        color sidebar_highlight black color8
+        color sidebar_divider color8 default
+        color sidebar_flagged red default
+        color sidebar_new green default
+      '';
     };
   };
 
