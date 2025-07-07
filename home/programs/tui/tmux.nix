d7275a766fd9a3bb701ff82b4e5f9fa904c16643
Pawel Lisewski <dev@palekiwi.com>
2025-07-07 16:27:46 +0800

    Tmux separator

--- home/programs/tui/tmux.nix
+++ home/programs/tui/tmux.nix
@@ -20,7 +20,7 @@ let
     prInfo = ''#[fg=green,dim,bold]#(cd #{pane_current_path}; [ -f .git/pr-info ] && source .git/pr-info && echo "#$GH_PR_NUMBER #[fg=white,nobold,dim]-> #[fg=white]$GIT_BASE" || echo "")'';
   };
 
-  statusLeft = with widgets; '' ${sessionName} ${gitIcon} ${gitBranch} ${prInfo} '';
+  statusLeft = with widgets; '' ${sessionName} ${gitIcon} ${gitBranch} ${prInfo}'';
 in
 {
   home.packages = with pkgs; [ tmux ];
@@ -79,13 +79,13 @@ in
       set -g status-left '${statusLeft}'
       set -g status-right ""
       set -g status-right-length 80
-      set -g status-left-length 80
+      set -g status-left-length 140
 
       setw -g window-status-current-style 'fg=colour15'
-      setw -g window-status-current-format '#W'
+      setw -g window-status-current-format ' |#W'
 
       setw -g window-status-style 'fg=colour7 dim'
-      setw -g window-status-format '#W'
+      setw -g window-status-format ' |#W'
 
       setw -g window-status-bell-style 'fg=colour2 bg=colour1 bold'
 
