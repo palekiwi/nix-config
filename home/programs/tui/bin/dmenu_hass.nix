97ba50e2cbe9740f367e434292f0e61472ca9881
Pawel Lisewski <dev@palekiwi.com>
2025-08-07 08:26:15 +0800

    Add fan_toggle to dmenu_hass

--- home/programs/tui/bin/dmenu_hass.nix
+++ home/programs/tui/bin/dmenu_hass.nix
@@ -14,7 +14,10 @@ let
 
   options = [
     "kitchen_off"
+    "fan_toggle"
   ] ++ lights;
+
+  plug_sonoff = "switch.0x00124b0026b87179";
 
   launcher = pkgs.writeShellScript "launcher" ''
     dmenu -i -nb \#1d1f21 -nf \#D3D7CF -sb \#37ADD4 -sf \#192330 -fn 11 -p "Home Assistant"
@@ -36,6 +39,9 @@ pkgs.writeShellScriptBin "dmenu_hass" ''
   case "$choice" in
     kitchen_off)
       ${hass} state turn_off ${lib.concatStringsSep " " lights_kitchen}
+    ;;
+    fan_toggle)
+      ${hass} state toggle ${plug_sonoff}
     ;;
     ${lib.concatMapStrings
       (light: ''
