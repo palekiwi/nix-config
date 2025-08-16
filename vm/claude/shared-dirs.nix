73871491f3d1af90003457bbc5187be7c023116b
Pawel Lisewski <dev@palekiwi.com>
2025-08-16 09:43:04 +0800

    Share Nextcloud with claude

--- vm/claude/shared-dirs.nix
+++ vm/claude/shared-dirs.nix
@@ -9,19 +9,25 @@
           target = "/mnt/documents";
         };
 
-        spabreaks = {
-          source = "$HOME/claude/shared/code/ygt/spabreaks";
-          target = "/mnt/code/ygt/spabreaks";
+        nextcloud = {
+          source = "$HOME/claude/Nextcloud";
+          target = "/mnt/Nextcloud";
         };
 
         labs = {
           source = "$HOME/code/palekiwi-labs";
           target = "/mnt/palekiwi-labs";
         };
+
+        spabreaks = {
+          source = "$HOME/claude/shared/code/ygt/spabreaks";
+          target = "/mnt/code/ygt/spabreaks";
+        };
       };
 
       fileSystems = {
         "/mnt/palekiwi-labs" = { options = [ "rw" ]; };
+        "/mnt/Nextcloud" = { options = [ "rw" ]; };
         "/mnt/code/ygt/spabreaks" = { options = [ "rw" ]; };
       };
     };
