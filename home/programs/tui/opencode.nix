b364d0699ca3da264e5e7c059b8e32d4d2bda710
Pawel Lisewski <dev@palekiwi.com>
2025-08-26 18:16:37 +0800

    Disable linking opencode config

--- home/programs/tui/opencode.nix
+++ home/programs/tui/opencode.nix
@@ -1,9 +1,9 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 
 {
   home.packages = with pkgs; [ opencode ];
 
-  home.file."${config.xdg.configHome}/agent-opencode/opencode.json" = {
-	source = ../../config/opencode/opencode.json;
-  };
+  # home.file."${config.xdg.configHome}/agent-opencode/opencode.json" = {
+  #   source = ../../config/opencode/opencode.json;
+  # };
 }
