c8d4643543c883da846657939f5e92ed2299a2eb
Pawel Lisewski <dev@palekiwi.com>
2025-08-28 11:00:06 +0800

    Split git config

--- home/programs/tui/git.nix
+++ home/programs/tui/git.nix
@@ -1,10 +1,7 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
-{
-  home.packages = with pkgs; [ git gitui ];
-
-  programs.git = {
-    enable = true;
+let
+  gitConfig = {
     userName = "Pawel Lisewski";
     userEmail = "dev@palekiwi.com";
     signing = {
@@ -14,10 +11,7 @@
     ignores = [
       "*.swp"
       ".direnv"
-      ".envrc"
-      ".gutctags"
       "build"
-      "gemset.nix"
       "log/test.log.0"
       "tags"
       "tags.lock"
@@ -25,12 +19,48 @@
       "tmux-client-*"
       "vendor"
     ];
-    # hooks = {
-    #   pre-commit = ../config/git/hooks/pre-commit;
-    # };
     extraConfig = {
       init.defaultBranch = "master";
       pull.rebase = true;
     };
   };
+
+  # Convert ignores list to gitignore format
+  globalIgnores = builtins.concatStringsSep "\n" gitConfig.ignores;
+
+in
+{
+  home.packages = with pkgs; [ git gitui ];
+
+  programs.git = gitConfig // { enable = true; };
+
+  # Org-specific files
+  home.file."code/ygt/.gitignore_org".text = ''
+    # Global ignores
+    ${globalIgnores}
+
+    # YGT-specific ignores
+      ".envrc"
+      ".gutctags"
+      "gemset.nix"
+      "AGENTS.md"
+      ".opencode"
+      ""
+  '';
+
+  home.file."code/ygt/.gitconfig_ygt".text = ''
+    [user]
+        name = ${gitConfig.userName}
+        email = ${gitConfig.userEmail}
+    [commit]
+        gpgsign = ${if gitConfig.signing.signByDefault then "true" else "false"}
+    [user]
+        signingkey = ${gitConfig.signing.key}
+    [init]
+        defaultBranch = ${gitConfig.extraConfig.init.defaultBranch}
+    [pull]
+        rebase = ${if gitConfig.extraConfig.pull.rebase then "true" else "false"}
+    [core]
+        excludesfile = ~/.config/home-manager/some-org/.gitignore_org
+  '';
 }
