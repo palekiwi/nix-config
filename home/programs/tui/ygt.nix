7c0c7d0a4cd68948ed7d78cb7224ac48d0449a78
Pawel Lisewski <dev@palekiwi.com>
2025-08-30 23:00:07 +0800

    Add home/config/ygt/sb-voucher-redemptions/.envrc

--- home/programs/tui/ygt.nix
+++ home/programs/tui/ygt.nix
@@ -25,8 +25,10 @@ in
       "code/ygt/.envrc".source = ../../config/ygt/.envrc;
       "code/ygt/.gitconfig".source = ../../config/ygt/.gitconfig;
       "code/ygt/.gitignore".text = import ../../config/ygt/.gitignore.nix;
-      "code/ygt/spabreaks/.envrc".source = ../../config/ygt/spabreaks/.envrc;
 
+      "code/ygt/sb-voucher-redemptions/.envrc".source = ../../config/ygt/sb-voucher-redemptions/.envrc;
+
+      "code/ygt/spabreaks/.envrc".source = ../../config/ygt/spabreaks/.envrc;
       "code/ygt/spabreaks/.git/hooks/pre-commit".source = ../../config/ygt/spabreaks/git/hooks/pre-commit;
       "code/ygt/spabreaks/.git/hooks/post-checkout".source = ../../config/ygt/spabreaks/git/hooks/post-checkout;
       "code/ygt/spabreaks/.git/hooks/post-merge".source = ../../config/ygt/spabreaks/git/hooks/post-merge;
