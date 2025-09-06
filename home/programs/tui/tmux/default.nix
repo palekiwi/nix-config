f5c509f01d7145edc7235949a1b94f6d885f0284
Pawel Lisewski <dev@palekiwi.com>
2025-09-06 19:44:38 +0800

    Migrate home/programs/tui/tmux/ygt-vrs-dev.nix

--- home/programs/tui/tmux/default.nix
+++ home/programs/tui/tmux/default.nix
@@ -3,4 +3,5 @@
 [
   (import ./git-repo.nix { inherit pkgs; })
   (import ./ygt-spabreaks-dev.nix { inherit pkgs; })
+  (import ./ygt-vrs-dev.nix { inherit pkgs; })
 ]
---
+++ home/programs/tui/tmux/ygt-vrs-dev.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+pkgs.writeShellScriptBin "_tmux_ygt_vrs_dev" ''
+  session="vrs-dev"
+
+  tmux rename-window -t $session:1 dev
+
+  tmux new-window -t $session -n vrs-db
+
+  tmux send-keys -t $session:2 'docker-compose up db' C-m
+  sleep 1
+  tmux send-keys -t $session:1 'make dev' C-m
+
+  tmux select-window -t $session:1
+''
