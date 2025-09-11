78aadee7342bb131026d04a7b5df52055d438cb0
Pawel Lisewski <dev@palekiwi.com>
2025-09-11 08:47:38 +0800

    Add home/programs/tui/dmenu/remote-tmux.nix

--- home/programs/tui/dmenu/default.nix
+++ home/programs/tui/dmenu/default.nix
@@ -5,6 +5,7 @@
   (import ./audio-sinks.nix { inherit pkgs; })
   (import ./hass.nix { inherit pkgs lib; })
   (import ./quit.nix { inherit pkgs; })
+  (import ./remote-tmux.nix { inherit pkgs; })
   (import ./tmux.nix { inherit pkgs; })
   (import ./xrandr.nix { inherit pkgs; })
 ]
