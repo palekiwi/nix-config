8c75a5239475a2ea74f71c906f12ffadf2f38935
Pawel Lisewski <dev@palekiwi.com>
2025-09-13 09:42:42 +0800

    Use port ranges for firewall-docker

--- hosts/pale/firewall-docker.nix
+++ hosts/pale/firewall-docker.nix
@@ -2,7 +2,11 @@
 {
   config = lib.mkIf config.modules.docker.enable {
     networking.firewall.interfaces."docker0" = {
-      allowedTCPPorts = [ 30301 33222 ];
+      allowedTCPPorts = [ 33222 ];
+
+      allowedTCPPortRanges = [
+       { from = 30300; to = 30309; }
+     ];
     };
   };
 }
