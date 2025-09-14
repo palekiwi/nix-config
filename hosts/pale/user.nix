df7f8c301cd920d585b8ce7704ef8808aec439b0
Pawel Lisewski <dev@palekiwi.com>
2025-09-14 11:26:52 +0800

    Authorize xiaomi-tab on pale

--- hosts/pale/user.nix
+++ hosts/pale/user.nix
@@ -5,6 +5,9 @@
     description = "pl";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;
+    openssh.authorizedKeys.keyFiles = [
+      ../../users/pl/ssh-xiaomi-tab.pub
+    ];
   };
 
   programs.zsh.enable = true;
