9472e885c75d0ca8724c81aa744c20483a18e540
Pawel Lisewski <dev@palekiwi.com>
2025-09-19 18:24:02 +0800

    Add home/users/pl/deck.nix

---
+++ home/users/pl/deck.nix
@@ -0,0 +1,26 @@
+{ ... }:
+
+{
+  imports = [
+    ../../programs/gui
+    ../../programs/tui
+  ];
+
+  config = {
+    home = {
+      username = "pl";
+      homeDirectory = "/home/pl";
+      stateVersion = "25.05";
+    };
+
+    nixpkgs.config.allowUnfree = true;
+    programs.home-manager.enable = true;
+
+    modules = {
+      chrome.enable = true;
+      kitty.enable = true;
+      oss.enable = true;
+      ygt.enable = true;
+    };
+  };
+}
