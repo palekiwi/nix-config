21a0e935090769adca7caf6bcb154f11aba8aa3d
Pawel Lisewski <dev@palekiwi.com>
2025-09-19 16:35:45 +0800

    Copy hosts/deck/configuration.nix

---
+++ hosts/deck/configuration.nix
@@ -0,0 +1,50 @@
+{ pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "deck";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Asia/Taipei";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "zh_TW.UTF-8";
+    LC_IDENTIFICATION = "zh_TW.UTF-8";
+    LC_MEASUREMENT = "zh_TW.UTF-8";
+    LC_MONETARY = "zh_TW.UTF-8";
+    LC_NAME = "zh_TW.UTF-8";
+    LC_NUMERIC = "zh_TW.UTF-8";
+    LC_PAPER = "zh_TW.UTF-8";
+    LC_TELEPHONE = "zh_TW.UTF-8";
+    LC_TIME = "zh_TW.UTF-8";
+  };
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  users.users.pl = {
+    isNormalUser = true;
+    description = "pl";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [ ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [];
+
+  services.openssh.enable = true;
+
+  system.stateVersion = "25.05";
+}
