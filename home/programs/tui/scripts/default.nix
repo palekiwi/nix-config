f4187e18fc9f826edf5c5e809e2673849111d618
Pawel Lisewski <dev@palekiwi.com>
2025-09-25 08:51:50 +0800

    Add home/programs/tui/scripts/opencode-cleanup.nix

--- home/programs/tui/scripts/default.nix
+++ home/programs/tui/scripts/default.nix
@@ -12,6 +12,7 @@
   (import ./gh_prs.nix { inherit pkgs; })
   (import ./hass.nix { inherit pkgs; })
   (import ./opencode-container-name.nix { inherit pkgs; })
+  (import ./opencode-cleanup.nix { inherit pkgs; })
   (import ./set_pr_info.nix { inherit pkgs; })
   (import ./sync_opencode_extra_config.nix { inherit pkgs; })
   (import ./yt-subs.nix { inherit pkgs lib config; })
