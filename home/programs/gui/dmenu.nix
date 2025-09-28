3b6ef6440beafa5c5d32090cb40eca1e727445bb
Pawel Lisewski <dev@palekiwi.com>
2025-09-28 09:41:59 +0800

    Move dmenu to /gui

--- home/programs/gui/dmenu.nix
+++ home/programs/gui/dmenu.nix
@@ -1,10 +1,12 @@
+{ pkgs, lib, ... }:
+
 {
+  home.packages = import ./dmenu { inherit pkgs lib; };
+
   home.file.".dmenu/agents".source = ../../scripts/dmenu/dmenu_agents.sh;
 
   home.file.".dmenu/hass".source = ../../scripts/dmenu/dmenu_hass.sh;
   home.file.".dmenu/mindmaps".source = ../../scripts/dmenu/dmenu_mindmaps.sh;
   home.file.".dmenu/process".source = ../../scripts/dmenu/dmenu_process.sh;
   home.file.".dmenu/run".source = ../../scripts/dmenu/dmenu_run.sh;
-
-
 }
