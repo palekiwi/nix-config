dc102c78b1d440805a17b5470b53243b944f96ea
Pawel Lisewski <dev@palekiwi.com>
2025-09-28 09:46:57 +0800

    Do not use mindmaps script

--- home/programs/gui/dmenu.nix
+++ home/programs/gui/dmenu.nix
@@ -4,7 +4,6 @@
   home.packages = import ./dmenu { inherit pkgs lib; };
 
   home.file.".dmenu/hass".source = ../../scripts/dmenu/dmenu_hass.sh;
-  home.file.".dmenu/mindmaps".source = ../../scripts/dmenu/dmenu_mindmaps.sh;
   home.file.".dmenu/process".source = ../../scripts/dmenu/dmenu_process.sh;
   home.file.".dmenu/run".source = ../../scripts/dmenu/dmenu_run.sh;
 }
