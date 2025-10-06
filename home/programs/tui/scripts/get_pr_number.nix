7f6c546d24b3e4f9717be087a33070e324f46bab
Pawel Lisewski <dev@palekiwi.com>
2025-10-06 14:59:42 +0800

    Fix home/programs/tui/scripts/get_pr_number.nix

--- home/programs/tui/scripts/get_pr_number.nix
+++ home/programs/tui/scripts/get_pr_number.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "get_pr_number" ''
-  if [[ -f .git/pr-info ]]; then
-    source .git/pr-info
-    echo "$GH_PR_NUMBER"
+  if [[ -f .git/GH_PR_NUMBER ]]; then
+    cat .git/GH_PR_NUMBER
+  else
+    exit 1
   fi
 ''
