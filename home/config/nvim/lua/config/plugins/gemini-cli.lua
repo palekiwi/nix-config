c6223fee7e301731ac53cb38fcc2676ed4820026
Pawel Lisewski <dev@palekiwi.com>
2025-10-07 12:31:56 +0800

    Update home/config/nvim/lua/config/plugins/gemini-cli.lua

--- home/config/nvim/lua/config/plugins/gemini-cli.lua
+++ home/config/nvim/lua/config/plugins/gemini-cli.lua
@@ -4,7 +4,7 @@ return {
     cmd = "Gemini",
     gemini_cmd = "gemini-cli",
     keys = {
-      { "<space>a/", "<cmd>Gemini toggle<cr>",   desc = "Toggle Gemini CLI" },
+      { "<space>g/", "<cmd>Gemini toggle<cr>",   desc = "Toggle Gemini CLI" },
       { "<space>ga", "<cmd>Gemini ask<cr>",      desc = "Ask Gemini",       mode = { "n", "v" } },
       { "<space>gf", "<cmd>Gemini add_file<cr>", desc = "Add File" },
 
