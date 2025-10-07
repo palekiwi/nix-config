c3a83601ffb272b2477a3231ace3fb0037a83e43
Pawel Lisewski <dev@palekiwi.com>
2025-10-07 12:22:10 +0800

    nvim: add plugin/gemini-cli

---
+++ home/config/nvim/lua/config/plugins/gemini-cli.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "marcinjahn/gemini-cli.nvim",
+    cmd = "Gemini",
+    gemini_cmd = "gemini-cli",
+    keys = {
+      { "<space>a/", "<cmd>Gemini toggle<cr>",   desc = "Toggle Gemini CLI" },
+      { "<space>ga", "<cmd>Gemini ask<cr>",      desc = "Ask Gemini",       mode = { "n", "v" } },
+      { "<space>gf", "<cmd>Gemini add_file<cr>", desc = "Add File" },
+
+    },
+    dependencies = {
+      "folke/snacks.nvim",
+    },
+    config = true,
+  }
+}
