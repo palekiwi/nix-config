e592899b8272520b72c03e2e0d87922259b482a4
Pawel Lisewski <dev@palekiwi.com>
2025-10-08 08:47:49 +0800

    nvim::opencode: add `pr:*` bindings

--- home/config/nvim/lua/config/plugins/opencode.lua
+++ home/config/nvim/lua/config/plugins/opencode.lua
@@ -15,14 +15,16 @@ return {
       { '<S-C-d>',    function() require('opencode').command('messages_half_page_down') end, desc = 'Scroll messages down', },
       { '<S-C-u>',    function() require('opencode').command('messages_half_page_up') end, desc = 'Scroll messages up', },
       { '<space>a', function() require('opencode').ask() end, desc = 'Ask opencode', },
-      { '<space>e', function() require('opencode').prompt("Explain @cursor and its context") end, desc = "Explain code near cursor", },
+      { '<space>ec', function() require('opencode').prompt("Explain @cursor and its context") end, desc = "Explain code near cursor", },
+      { '<space>ep', function() require('opencode').command('pr:explain') end, desc = 'PR: Explain', },
+      { '<space>er', function() require('opencode').command('pr:fusion-review') end, desc = 'PR: Fusion Review', },
       { '<space>n', function() require('opencode').command('session_new') end, desc = 'New session', },
       { '<space>p', function() require('opencode').select() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
       { '<space>i', function() require('opencode').ask('@selection: ') end, desc = 'Ask opencode about selection', mode = 'v', },
       { '<space>i', function() require('opencode').ask('@cursor: ') end, desc = 'Ask at cursor', mode = 'n', },
       { '<space>f', function() require('opencode').ask('@buffer: ') end, desc = 'Ask about buffer', mode = 'n', },
       { '<space>y', function() require('opencode').command('messages_copy') end, desc = 'Copy last message', },
-      { '<space>rb', function() require('opencode').prompt('@spec-writer @buffer: fix these: @diagnostics ') end, desc = 'Fix specs for the buffer', },
+      { '<space>rb', function() require('opencode').prompt('Fix: @diagnostics ') end, desc = 'Fix diagnostics', },
     },
   }
 }
