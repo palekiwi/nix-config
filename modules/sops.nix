48c6d5bfa7d2ded69d48216f1fb5ba8f4337c4d5
Pawel Lisewski <dev@palekiwi.com>
2025-10-09 12:20:40 +0800

    Add airbrake key

--- modules/sops.nix
+++ modules/sops.nix
@@ -5,13 +5,14 @@
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/pl/.config/sops/age/keys.txt";
 
+  sops.secrets."airbrake/api_key" = { owner = "pl"; };
+  sops.secrets."github/prompt-assist" = { owner = "pl"; };
   sops.secrets."hass/server" = { owner = "pl"; };
   sops.secrets."hass/token" = { owner = "pl"; };
-  sops.secrets."jira/token" = { owner = "pl"; };
   sops.secrets."jira/email" = { owner = "pl"; };
-  sops.secrets."github/prompt-assist" = { owner = "pl"; };
-  sops.secrets."zai_coding_plan/api_key" = { owner = "pl"; };
+  sops.secrets."jira/token" = { owner = "pl"; };
   sops.secrets."opencode/api_key" = { owner = "pl"; };
+  sops.secrets."zai_coding_plan/api_key" = { owner = "pl"; };
 
   sops.secrets."gotify/token" = { owner = "notifications-server"; };
 }
