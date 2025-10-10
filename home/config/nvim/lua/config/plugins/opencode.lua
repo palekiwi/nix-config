fec559ebef2a6973cfd874072101d70850e7647a
Pawel Lisewski <dev@palekiwi.com>
2025-10-10 09:36:19 +0800

    Fix quotes

--- home/config/nvim/lua/config/plugins/opencode.lua
+++ home/config/nvim/lua/config/plugins/opencode.lua
@@ -35,17 +35,17 @@ return {
 
       local opts = { submit = true }
       local keymaps = {
-        { "n", "<space>a",  function() oc.ask() end,                                         'Ask opencode', },
+        { "n", "<space>a",  function() oc.ask() end,                                         "Ask opencode", },
         { "n", "<space>et", function() oc.prompt("Explain @this and its context", opts) end, "Explain this" },
         { "n", "<space>i",  function() oc.ask("@this: ", opts) end,                          "Ask about this" },
         { "n", "<space>f",  function() oc.ask("@buffer: ", opts) end,                        "Ask about buffer" },
         { "n", "<space>d",  function() oc.ask("@diff: ", opts) end,                          "Ask about diff" },
         { "n", "<space>n",  function() oc.command("session_new") end,                        "New session" },
-        { "n", "<space>o+", function() oc.prompt("@this") end,                               "Add this" },
-        { "n", "<space>pe", function() oc.prompt('/pr:explain', opts) end,                   'PR: Explain' },
-        { "n", "<space>pr", function() oc.prompt('/pr:fusion-review', opts) end,             'PR: Fusion Review' },
+        { "n", "<space>+",  function() oc.prompt("@this") end,                               "Add this" },
+        { "n", "<space>pe", function() oc.prompt("/pr:explain", opts) end,                   "PR: Explain" },
+        { "n", "<space>pr", function() oc.prompt("/pr:fusion-review", opts) end,             "PR: Fusion Review" },
         { "n", "<space>s",  function() oc.select() end,                                      "Select prompt" },
-        { "n", "<space>y",  function() oc.command('messages_copy') end,                      'Copy last message' },
+        { "n", "<space>y",  function() oc.command("messages_copy") end,                      "Copy last message" },
       }
 
       for _, m in ipairs(keymaps) do
