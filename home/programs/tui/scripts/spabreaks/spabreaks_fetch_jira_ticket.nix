b882e082569cadfd0617b954d55ca96552c91e25
Pawel Lisewski <dev@palekiwi.com>
2025-11-04 16:51:06 +0800

    Use .nu script in home/programs/tui/scripts/spabreaks/spabreaks_fetch_jira_ticket.nix

--- home/programs/tui/scripts/spabreaks/spabreaks_fetch_jira_ticket.nix
+++ home/programs/tui/scripts/spabreaks/spabreaks_fetch_jira_ticket.nix
@@ -1,13 +1,3 @@
 { pkgs, ... }:
 
-pkgs.writeShellScriptBin "spabreaks_fetch_jira_ticket" ''
-  # Use provided ticket or get from branch
-  TICKET="''${1:-$(spabreaks_jira_ticket_from_branch)}"
-
-  # Construct the API URL
-  API_URL="$JIRA_URL/rest/api/2/issue/$TICKET"
-
-  # Make the API request and extract the description
-  curl -s -u "$JIRA_EMAIL:$JIRA_TOKEN" -X GET -H "Content-Type: application/json" "$API_URL?expand=renderedFields" \
-    | jq -r '{title: .fields.summary, description: .renderedFields.description, technical_notes: .renderedFields.customfield_10136}'
-''
+pkgs.writers.writeNuBin "spabreaks_fetch_jira_ticket" (builtins.readFile ./sb_fetch_jira_ticket.nu)
