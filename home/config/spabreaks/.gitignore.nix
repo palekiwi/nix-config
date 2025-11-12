6f868e64ee8d1aecbee57c64b3dedf78eb76ef06
Pawel Lisewski <dev@palekiwi.com>
2025-11-12 13:59:23 +0800

    Add home/config/spabreaks/

---
+++ home/config/spabreaks/.gitignore.nix
@@ -0,0 +1,20 @@
+let
+  globalIgnores = builtins.concatStringsSep "\n" (import ../../programs/tui/gitignores.nix);
+in
+
+''
+  # Global ignores
+  ${globalIgnores}
+
+  # Spabreaks-specific ignores
+  .envrc
+  .gutctags
+  .opencode
+  .agents
+  AGENTS.md
+  ai_docs
+  gemset.nix
+  opencode.json
+  cypress/cypress/screenshots
+  cypress/cypress/snapshots
+''
