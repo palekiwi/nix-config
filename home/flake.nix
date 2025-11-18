e047878d39ffc57662c39cda4558a69c6f337fe1
Pawel Lisewski <dev@palekiwi.com>
2025-11-18 14:27:04 +0800

    home/flake.lock

--- home/flake.nix
+++ home/flake.nix
@@ -8,7 +8,7 @@
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    mcp-rspec.url = "github:palekiwi/mcp-rspec/eb59815172d74b8eb25913f89200a1b033265596";
+    mcp-rspec.url = "github:palekiwi/mcp-rspec/f4bf92f6f859ab35b060a3a932e7b7930d31d504";
     test-runner-mcp.url = "github:palekiwi-labs/test-runner-mcp/13d05835c8d8a3829a6f07776b6e646571944ab3";
     wrappedAgents.url = "github:palekiwi-labs/agents/137e07883605385f8989bbe96849a3da82923df9";
   };
