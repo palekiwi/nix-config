eccfddf1443a4caa5d52c5481ac2f6062d92a4be
Pawel Lisewski <dev@palekiwi.com>
2025-11-19 16:57:56 +0800

    Switch to new agents

--- home/flake.nix
+++ home/flake.nix
@@ -10,7 +10,7 @@
     };
     mcp-rspec.url = "github:palekiwi/mcp-rspec/f4bf92f6f859ab35b060a3a932e7b7930d31d504";
     test-runner-mcp.url = "github:palekiwi-labs/test-runner-mcp/13d05835c8d8a3829a6f07776b6e646571944ab3";
-    wrappedAgents.url = "github:palekiwi-labs/agents/137e07883605385f8989bbe96849a3da82923df9";
+    wrappedAgents.url = "github:palekiwi-labs/agents";
   };
 
   outputs = { nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
