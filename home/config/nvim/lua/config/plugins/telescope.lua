69a2ac07c5396255792a6556c0fa0432dd2ee5de
Pawel Lisewski <dev@palekiwi.com>
2025-11-22 15:05:24 +0800

    telescope: add layout cycling with <C-x>

--- home/config/nvim/lua/config/plugins/telescope.lua
+++ home/config/nvim/lua/config/plugins/telescope.lua
@@ -28,6 +28,7 @@ return {
             i = {
               ["<C-a>"] = actions.cycle_previewers_prev,
               ["<C-p>"] = actions_layout.toggle_preview,
+              ["<C-x>"] = actions_layout.cycle_layout_next,
               ["<C-s>"] = actions.cycle_previewers_next,
             }
           },
@@ -35,17 +36,17 @@ return {
           layout_strategy = "flex",
           layout_config = {
             horizontal = {
-              height = 0.9,
+              height = 0.95,
               preview_cutoff = 120,
               prompt_position = "top",
-              width = 0.9
+              width = 0.95
             },
             vertical = {
               height = 0.95,
               mirror = true,
-              prompt_position = "top",
               preview_cutoff = 0,
               preview_height = 0.65,
+              prompt_position = "top",
               width = 0.95
             }
           }
