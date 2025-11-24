48e9481879b7ad4f9a3050da5b0e42d9b7c3bd0f
Pawel Lisewski <dev@palekiwi.com>
2025-11-24 13:30:08 +0800

    Remove root duplicate from trusted users

--- modules/cachix.nix
+++ modules/cachix.nix
@@ -4,6 +4,6 @@
   nix.settings = {
     substituters = [ "https://palekiwi.cachix.org" ];
     trusted-public-keys = [ "palekiwi.cachix.org-1:/S23j64quRTMpe+zteCPAd0p8fczVTWzOpV5mFoFOg8=" ];
-    trusted-users = [ "root" "pl" ];
+    trusted-users = [ "pl" ];
   };
 }
