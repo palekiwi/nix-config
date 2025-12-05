b999cc8cebee4dea1a4bc4493526f1faeab6315e
Pawel Lisewski <dev@palekiwi.com>
2025-12-05 14:17:42 +0800

    Wrap taskwarrior-tui to always find task

--- home/programs/tui/utils.nix
+++ home/programs/tui/utils.nix
@@ -1,5 +1,18 @@
 { pkgs, lib, ... }:
 
+let
+  # Wrap taskwarrior-tui to ensure it always finds taskwarrior3's task binary,
+  # even when devshells bring in their own go-task that overrides PATH
+  taskwarrior-tui-wrapped = pkgs.symlinkJoin {
+    name = "taskwarrior-tui-wrapped";
+    paths = [ pkgs.taskwarrior-tui ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/taskwarrior-tui \
+        --prefix PATH : ${pkgs.taskwarrior3}/bin
+    '';
+  };
+in
 {
   home.packages = with pkgs; [
     acpi
@@ -20,7 +33,7 @@
     ranger
     ripgrep
     starship
-    taskwarrior-tui
+    taskwarrior-tui-wrapped
     taskwarrior3
     tldr
     tree
