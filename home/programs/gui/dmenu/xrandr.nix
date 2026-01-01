92b43738d733847683857617480f280982b8aa85
Pawel Lisewski <dev@palekiwi.com>
2026-01-01 19:34:45 +0800

    Refactor with data

--- home/programs/gui/dmenu/xrandr.nix
+++ home/programs/gui/dmenu/xrandr.nix
@@ -7,14 +7,47 @@ pkgs.writers.writeNuBin "dmenu_xrandr" ''
       tablet: "DP-1-3"
     }
 
+    let pale_actions = {
+       builtin: { ||
+         xrandr --output $pale.builtin --auto --primary
+         xrandr --output $pale.external --off
+         xrandr --output $pale.tablet --off
+       }
+       external: { ||
+         xrandr --output $pale.external --auto --primary
+         xrandr --output $pale.builtin --off
+         xrandr --output $pale.tablet --off
+       }
+       dual: { ||
+         xrandr --output $pale.builtin --auto --primary
+         xrandr --output $pale.external --off
+         xrandr --output $pale.tablet --auto --left-of $pale.builtin --rotate inverted
+       }
+       "dual-external": { ||
+         xrandr --output $pale.external --auto --primary
+         xrandr --output $pale.builtin --off
+         xrandr --output $pale.tablet --auto --pos 760x1440 --rotate inverted
+       }
+    }
+
     let deck = {
       builtin: "eDP-1"
       external: "DisplayPort-0"
     }
 
-    let opts = {
-      pale: ["builtin", "external", "dual", "dual-external"]
-      deck: ["builtin", "external", "dual"]
+    let deck_actions = {
+       builtin: { ||
+         xrandr --output $deck.builtin --rotate right --auto --primary
+         xrandr --output $deck.external --off
+       }
+       external: { ||
+         xrandr --output $deck.external --auto --primary
+         xrandr --output $deck.builtin --off
+       }
+       dual: { ||
+         xrandr --output $deck.external --auto --primary
+         xrandr --output $deck.builtin --rotate right --auto --below $deck.external
+       }
     }
 
     def restart_wm [] {
@@ -25,63 +58,16 @@ pkgs.writers.writeNuBin "dmenu_xrandr" ''
       $opts | dmenu -i -nb "#1d1f21" -nf "#D3D7CF" -sb "#5294e2" -sf "#2f343f" -fn "11" -p "xrandr profile: "
     }
 
-    let host = (hostname | str trim)
-
-    match $host {
+    match (hostname | str trim) {
       "deck" => {
-        let choice = ($options_deck | str join "\n" | run_dmenu $in)
-        match $choice {
-          "builtin" => {
-            xrandr --output $deck_builtin --rotate right --auto --primary
-            xrandr --output $deck_external --off
-            restart_wm
-          }
-          "external" => {
-            xrandr --output $deck_external --auto --primary
-            xrandr --output $deck_builtin --off
-            restart_wm
-          }
-          "dual" => {
-            xrandr --output $deck_external --auto --primary
-            xrandr --output $deck_builtin --rotate right --auto --below $deck_external
-            restart_wm
-          }
-          _ => {
-            exit 1
-          }
-        }
+         let choice = ($deck_actions | columns | str join "\n" | run_dmenu $in)
+         let action = ($deck_actions | get -o $choice)
+         if $action != null { do $action; restart_wm } else { exit 1 }
       }
       "pale" => {
-        let choice = ($options_pale | str join "\n" | run_dmenu $in)
-        match $choice {
-          "builtin" => {
-            xrandr --output $pale.builtin --auto --primary
-            xrandr --output $pale.external --off
-            xrandr --output $pale.tablet --off
-            restart_wm
-          }
-          "external" => {
-            xrandr --output $pale.external --auto --primary
-            xrandr --output $pale.builtin --off
-            xrandr --output $pale.tablet --off
-            restart_wm
-          }
-          "dual" => {
-            xrandr --output $pale.builtin --auto --primary
-            xrandr --output $pale.external --off
-            xrandr --output $pale.tablet --auto --left-of $pale.builtin --rotate inverted
-            restart_wm
-          }
-          "dual-external" => {
-            xrandr --output $pale.external --auto --primary
-            xrandr --output $pale.builtin --off
-            xrandr --output $pale.tablet --auto --pos 760x1440 --rotate inverted
-            restart_wm
-          }
-          _ => {
-            exit 1
-          }
-        }
+         let choice = ($pale_actions | columns | str join "\n" | run_dmenu $in)
+         let action = ($pale_actions | get -o $choice)
+         if $action != null { do $action; restart_wm } else { exit 1 }
       }
       _ => {
         notify-send "xrandr unavailable on this host"
