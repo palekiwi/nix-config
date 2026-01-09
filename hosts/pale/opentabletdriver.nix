e896ed799f1f34deb59a33d72801560ce509d6c7
Pawel Lisewski <dev@palekiwi.com>
2026-01-09 23:58:04 +0800

    [WIP] Working pressure sensitivity in rnote

--- hosts/pale/opentabletdriver.nix
+++ hosts/pale/opentabletdriver.nix
@@ -10,4 +10,17 @@
   # Required by OpenTabletDriver
   hardware.uinput.enable = true;
   boot.kernelModules = [ "uinput" ];
+
+  services.xserver.modules = [ pkgs.xf86_input_wacom ];
+
+  users.users.pl.extraGroups = [ "input" ];
+
+  services.xserver.config = ''
+    Section "InputClass"
+      Identifier "OpenTabletDriver Virtual Tablet"
+      MatchProduct "OpenTabletDriver Virtual"
+      MatchDevicePath "/dev/input/event*"
+      Driver "wacom"
+    EndSection
+  '';
 }
