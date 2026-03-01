17ecfb2989e44698f0f44966352f2900e93fc832
Pawel Lisewski <dev@palekiwi.com>
2026-03-01 13:45:40 +0800

    home/programs/gui/dmenu/process.nix: handy

--- home/programs/gui/dmenu/process.nix
+++ home/programs/gui/dmenu/process.nix
@@ -1,12 +1,15 @@
 { pkgs }:
 
 pkgs.writers.writeNuBin "dmenu_process" ''
-  let options = ["opentabletdriver" "sxhkd" "xmodmap" "picom"]
+  let options = ["handy" "opentabletdriver" "sxhkd" "xmodmap" "picom"]
   let dmenu_options = ["-i" "-nb" "#1d1f21" "-nf" "#D3D7CF" "-sb" "#ffb05f" "-sf" "#192330" "-fn" "11" "-p" "Restart process: "]
 
   let choice = ($options | str join "\n" | run-external "dmenu" ...$dmenu_options)
 
   match $choice {
+    "handy" => {
+      systemctl --user restart handy
+    },
     "opentabletdriver" => {
       systemctl --user restart opentabletdriver
     },
