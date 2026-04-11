7d5a27fc000ed512148c1b87d254856e54c8f973
Pawel Lisewski <dev@palekiwi.com>
2026-04-11 14:48:18 +0800

    Spabreaks things

--- home/programs/tui/tmux/spabreaks-vrs-dev.nix
+++ home/programs/tui/tmux/spabreaks-vrs-dev.nix
@@ -4,8 +4,10 @@ pkgs.writeShellScriptBin "_tmux_spabreaks_vrs_dev" ''
   session="vrs-dev"
 
   tmux rename-window -t $session:1 dev
+  tmux new-window -t $session -n mcp-rspec
 
   tmux send-keys -t $session:1 'make dev' C-m
+  tmux send-keys -t $session:2 'task mcp:rspec' C-m
 
   tmux select-window -t $session:1
 ''
