c9e47bf0a82a06e3cc7abd76d64e04192de52679
Pawel Lisewski <dev@palekiwi.com>
2026-04-11 21:24:34 +0800

    Customize LspInlayHints

--- home/config/nvim/lua/config/setup/highlights.lua
+++ home/config/nvim/lua/config/setup/highlights.lua
@@ -4,6 +4,12 @@ local function stimulus_hightlights()
   vim.api.nvim_set_hl(0, "@stimulus-attribute.html", { fg = color.fg, bold = false })
 end
 
+local function lsp_highlights()
+  local color = vim.api.nvim_get_hl(0, { name = "Comment" })
+  vim.api.nvim_set_hl(0, "LspInlayHint", { fg = color.fg, bg = "none", italic = true })
+end
+
 return function()
   stimulus_hightlights()
+  lsp_highlights()
 end
