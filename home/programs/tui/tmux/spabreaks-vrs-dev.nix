0aad0e9e61c290593445b73ab76f70a5bb772622
Pawel Lisewski <dev@palekiwi.com>
2026-04-28 21:50:04 +0800

    Fix home/programs/tui/tmux/spabreaks-vrs-dev.nix

--- home/programs/tui/tmux/spabreaks-vrs-dev.nix
+++ home/programs/tui/tmux/spabreaks-vrs-dev.nix
@@ -4,10 +4,13 @@ pkgs.writeShellScriptBin "_tmux_spabreaks_vrs_dev" ''
   session="vrs-dev"
 
   tmux rename-window -t $session:1 dev
+
+  tmux new-window -t $session -n debug
   tmux new-window -t $session -n mcp-rspec
 
   tmux send-keys -t $session:1 'make dev' C-m
-  tmux send-keys -t $session:2 'task mcp:rspec' C-m
+  tmux send-keys -t $session:2 'sleep 3sec; make debug-web' C-m
+  tmux send-keys -t $session:3 'task mcp:rspec' C-m
 
   tmux select-window -t $session:1
 ''
