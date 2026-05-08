87455b7a9b9d12f1f91bb42f22ac7782b911a191
Pawel Lisewski <dev@palekiwi.com>
2026-05-08 11:10:38 +0800

    pin cast to instrumentation commit

--- home/flake.nix
+++ home/flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     mcp-rspec.url = "github:palekiwi/mcp-rspec";
-    cast.url = "github:palekiwi-labs/cast";
+    cast.url = "github:palekiwi-labs/cast/91b93e963a6caa2e37ad7a3390b0ed0afb591292";
     mem.url = "github:palekiwi-labs/mem";
     ocx.url = "github:palekiwi-labs/ocx";
     test-runner-mcp.url = "github:palekiwi-labs/test-runner-mcp/13d05835c8d8a3829a6f07776b6e646571944ab3";
