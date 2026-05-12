c0cac3fabb4a559dd97864a0cefcb62f4bf51ea0
Pawel Lisewski <dev@palekiwi.com>
2026-05-12 18:32:39 +0800

    spabreaks stuff

--- home/programs/tui/tmux/spabreaks-wss-dev.nix
+++ home/programs/tui/tmux/spabreaks-wss-dev.nix
@@ -9,7 +9,7 @@ pkgs.writeShellScriptBin "_tmux_spabreaks_wss_dev" ''
   tmux new-window -t $session -n mcp-rspec
 
   tmux send-keys -t $session:1 'task dev' C-m
-  tmux send-keys -t $session:3 'task mcp:rspec' C-m
+  tmux send-keys -t $session:3 'cast mcp start' C-m
 
   tmux select-window -t $session:1
 ''
