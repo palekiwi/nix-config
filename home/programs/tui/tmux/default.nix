dd47a61a6579d1844f5b9c81f98ec528c34c0f06
Pawel Lisewski <dev@palekiwi.com>
2026-05-20 14:24:10 +0800

    sesh: booking-transform

--- home/programs/tui/tmux/default.nix
+++ home/programs/tui/tmux/default.nix
@@ -5,6 +5,7 @@
   (import ./spabreaks-spabreaks-dev.nix { inherit pkgs; })
   (import ./spabreaks-vrs-dev.nix { inherit pkgs; })
   (import ./spabreaks-blog-dev.nix { inherit pkgs; })
+  (import ./spabreaks-booking-transform-dev.nix { inherit pkgs; })
   (import ./spabreaks-my-account-dev.nix { inherit pkgs; })
   (import ./spabreaks-wss-dev.nix { inherit pkgs; })
   (import ./spabreaks-wss-data-dev.nix { inherit pkgs; })
