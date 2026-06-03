c991244f1737354c3132f033709aef1c6fc52a7d
Pawel Lisewski <dev@palekiwi.com>
2026-06-03 12:03:08 +0800

    home/config/nvim/lua/config/setup/highlights.lua

--- home/config/nvim/lua/config/setup/highlights.lua
+++ home/config/nvim/lua/config/setup/highlights.lua
@@ -15,18 +15,18 @@ local function markdown_highlights()
 end
 
 local function mem_highlights()
-  vim.api.nvim_set_hl(0, "MemCategorySpec", { fg = "#B48EAD", bold = true })  -- purple
-  vim.api.nvim_set_hl(0, "MemCategoryPlan", { fg = "#B48EAD", bold = true })  -- purple
-  vim.api.nvim_set_hl(0, "MemCategoryTodo", { fg = "#81A1C1", bold = true })  -- blue
-  vim.api.nvim_set_hl(0, "MemCategoryDoc", { fg = "#D08770", bold = true })   -- orange
-  vim.api.nvim_set_hl(0, "MemCategoryTrace", { fg = "#A3BE8C", bold = true }) -- green
-  vim.api.nvim_set_hl(0, "MemCategoryBin", { fg = "#BF616A", bold = true })   -- red
-  vim.api.nvim_set_hl(0, "MemCategoryTmp", { fg = "#4C566A", bold = true })   -- grey
-  vim.api.nvim_set_hl(0, "MemCategoryRef", { fg = "#4C566A", bold = true })   -- grey
+  vim.api.nvim_set_hl(0, "MemCategorySpec", { fg = "#ff7b72", bold = false })  -- pink
+  vim.api.nvim_set_hl(0, "MemCategoryPlan", { fg = "#bc8cff", bold = false })  -- purple
+  vim.api.nvim_set_hl(0, "MemCategoryTodo", { fg = "#58a6ff", bold = false })  -- blue
+  vim.api.nvim_set_hl(0, "MemCategoryDoc", { fg = "#d29922", bold = false })   -- orange
+  vim.api.nvim_set_hl(0, "MemCategoryTrace", { fg = "#39c5cf", bold = false }) -- cyan
+  vim.api.nvim_set_hl(0, "MemCategoryBin", { fg = "#f85149", bold = false })   -- red
+  vim.api.nvim_set_hl(0, "MemCategoryTmp", { fg = "#4C566A", bold = false })   -- grey
+  vim.api.nvim_set_hl(0, "MemCategoryRef", { fg = "#4C566A", bold = false })   -- grey
 end
 
 return function()
-  -- stimulus_hightlights()
+  stimulus_hightlights()
   lsp_highlights()
   markdown_highlights()
   mem_highlights()
