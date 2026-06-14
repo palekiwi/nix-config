f579c2fdeecee6809ddea22dc3e5ede6a537e83c
Pawel Lisewski <dev@palekiwi.com>
2026-06-14 11:10:13 +0800

    flake.lock

--- home/flake.nix
+++ home/flake.nix
@@ -10,7 +10,7 @@
     };
     mcp-rspec.url = "github:palekiwi/mcp-rspec";
     cast.url = "github:palekiwi-labs/cast/dev";
-    mem.url = "github:palekiwi-labs/mem/a37c4625ec526eae41c3d1bea37930b9e75f9cc1";
+    mem.url = "github:palekiwi-labs/mem/2a49ad52189ed96930765fc9d117268f7a2970c8";
     ocx.url = "github:palekiwi-labs/ocx";
     test-runner-mcp.url = "github:palekiwi-labs/test-runner-mcp/13d05835c8d8a3829a6f07776b6e646571944ab3";
     handy.url = "github:cjpais/Handy";
