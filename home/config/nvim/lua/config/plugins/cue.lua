5e08d7e7a81783ec998beb95cdaa2a9b030c5787
Pawel Lisewski <dev@palekiwi.com>
2026-06-21 01:12:34 +0800

    feat: pin cue.nvim to master branch
    Ensures that even in dev mode, lazy.nvim prioritizes the master branch to avoid sourcing broken feature branches during plugin development.

--- home/config/nvim/lua/config/plugins/cue.lua
+++ home/config/nvim/lua/config/plugins/cue.lua
@@ -3,6 +3,7 @@ return {
     "palekiwi-labs/cue.nvim",
     dev = true,
     dir = "~/code/palekiwi-labs/cue.nvim",
+    branch = "master",
     dependencies = {
       "nvim-telescope/telescope.nvim",
       "folke/snacks.nvim",
