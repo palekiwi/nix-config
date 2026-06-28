4a2aee081eb578527eedab16118d4430d024af6b
Pawel Lisewski <dev@palekiwi.com>
2026-06-28 13:00:07 +0800

    home/programs/gui/sxhkd.nix

--- home/programs/gui/sxhkd.nix
+++ home/programs/gui/sxhkd.nix
@@ -87,8 +87,8 @@ in
       "super + l + control" = "dmenu_activity_log --pr";
 
       "super + space; t; e" = "${switchToSession} cue";
-      "super + space; t; c" = "${switchToSession} cast";
-      "super + space; t; n" = "${switchToSession} cue-nvim";
+      "super + space; t; n" = "${switchToSession} cast";
+      "super + space; t; v" = "${switchToSession} cue-nvim";
       "super + space; t; t" = "${switchToSession} taskwarrior";
       "super + space; t; u" = "${switchToSession} cue-plugins";
 
