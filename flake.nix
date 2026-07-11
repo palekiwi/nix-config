265db6b2e69c46e8bb3b6593ecd4ab99b12c6c10
Pawel Lisewski <dev@palekiwi.com>
2026-07-11 13:16:33 +0800

    flake: bump nixpkgs and home-manager to 26.05
    Bump both flakes from nixos-25.11 to nixos-26.05:
    - flake.nix: nixpkgs + home-manager
    - home/flake.nix: nixpkgs + home-manager
    
    sops-nix, claude-desktop, notifications-server all follow
    nixpkgs via inputs.nixpkgs.follows, no changes needed.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     notifications-server = {
