65a4a10a68c3a0cb0f3f31281e5601a397da1eed
Pawel Lisewski <dev@palekiwi.com>
2026-08-02 00:30:32 +0800

    home/programs/tui/tmux/spabreaks-wss-dev.nix

--- home/programs/tui/tmux/spabreaks-wss-dev.nix
+++ home/programs/tui/tmux/spabreaks-wss-dev.nix
@@ -6,7 +6,7 @@ pkgs.writeShellScriptBin "_tmux_spabreaks_wss_dev" ''
   tmux rename-window -t $session:1 dev
 
   tmux new-window -t $session -n debug
-  tmux new-window -t $session -n mcp-rspec
+  tmux new-window -t $session -n cast-mcp
 
   tmux send-keys -t $session:1 'task dev' C-m
   tmux send-keys -t $session:3 'cast mcp start' C-m
