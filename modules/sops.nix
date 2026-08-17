dcadbd970935c745b9fcd4f9285128e22f2e60af
Pawel Lisewski <dev@palekiwi.com>
2026-08-17 23:21:59 +0800

    sops: palekiwi/gh_cli_readonly

--- modules/sops.nix
+++ modules/sops.nix
@@ -9,6 +9,7 @@
   sops.secrets."cachix/personal/token" = { owner = "pl"; };
   sops.secrets."cachix/github_actions/token" = { owner = "pl"; };
   sops.secrets."github/palekiwi/gh_cli" = { owner = "pl"; };
+  sops.secrets."github/palekiwi/gh_cli_readonly" = { owner = "pl"; };
   sops.secrets."hass/server" = { owner = "pl"; };
   sops.secrets."hass/token" = { owner = "pl"; };
   sops.secrets."jira/email" = { owner = "pl"; };
